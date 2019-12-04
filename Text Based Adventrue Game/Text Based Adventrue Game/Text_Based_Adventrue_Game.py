6571b5b2ce4f85ab54f320656901b50db6f1e3b8
Neg999 <enegocomley@gmail.com>
2019-12-04 15:01:32 +0000

    Started on the first corridor

--- Text Based Adventrue Game/Text Based Adventrue Game/Text_Based_Adventrue_Game.py
+++ Text Based Adventrue Game/Text Based Adventrue Game/Text_Based_Adventrue_Game.py
@@ -1,14 +1,28 @@
  
 import random
-location = [1,1]
-mostRecentInput = ""
+location = [1,0]
 setting = 0
 roomLayout = [["1","2","3"],["1","2","3"],["1","2","3"]]
 hasKey = False
 playerName = ""
+endCode = "0"
+#commands = {"temp":0}
 
 def Room1_0():
+    print("\n")
+    print("\n")
+    print("\n")
     print("You find yourself at a juction, infront of you there are three doors labeld 1,2 and 3")
+    print("On the wall to the left of you is grafetied ", str(endCode[0]), str(endCode[1]), str(endCode[2]), str(endCode[3]))
+    commandToEnter = (input("What would you like to do?\n")).lower()
+    while (len(commandToEnter) != 0):
+        if commandToEnter in commands:
+            commands[commandToEnter]()
+            commandToEnter = ""
+        else:
+            print("That is not a command!")
+            commandToEnter = (input("What would you like to do?\n")).lower()
+
 
 def Room1_1():
     print("As you open the door infront of you, you notice a table, and lying on that table is a shiny golden key")
@@ -22,7 +36,7 @@ def Room1_3():
 
 
 def Room2_0():
-    print("You find yourself at a juction, infront of you there are three doors labeld 1,2 and 3")
+    print("You find yourself at a juction, infront of you there are three doors,  labeld 1,2 and 3")
 
 def Room2_1():
     print("In this room you find a tied up human")
@@ -53,28 +67,33 @@ def Room3_3():
 def GoBack():
     if (location[1] == 0):
         if (location[0] != 10):
-            location[0] = str((int)(location[0] - 1))
+            location[0] = (int)(location[0] - 1)
         else:
             print("The door behind you is locked")
             return
     else:
         location[1] = 0;
-    rooms[location[0]][location[1]]()
+    rooms[location[0] - 1][location[1]]()
 
 def EnterRoom():
-    if (mostRecentInput[len(mostRecentInput)-1] != "1" and mostRecentInput[len(mostRecentInput)-1] != "2" and mostRecentInput[len(mostRecentInput)-1] != "3"):
-        print (mostRecentInput[len(mostRecentInput)-1], "Is not a room")
-        return
-    if (location[1] == 0):
-        if (location[0] == 3):
-            if (mostRecentInput[len(mostRecentInput)-1] == "3"):
-                print("There is no third room")
-                return
-        location[1] = roomLayout[location[0]][mostRecentInput[len(mostRecentInput)-1]]
-        rooms[location[0]][location[1]]()
-    else:
-        print("I am sorry to tell you that you can't walk through walls")
-        return
+    while True:
+        roomToEnter = input("What room would you like to enter?\n")
+        if (roomToEnter != "1" and roomToEnter != "2" and roomToEnter != "3"):
+            print (roomToEnter, "Is not a room")
+            
+        if (location[1] == 0):
+            if (location[0] == 3):
+                if (roomToEnter == "3"):
+                    print("There is no third room")
+                    
+            location[1] = int(roomLayout[location[0]][(int)(roomToEnter) -1])
+            rooms[location[0]-1][location[1]]()
+            commands[(input("What would you like to do?\n")).lower()]()
+            return
+        else:
+            print("I am sorry to tell you that you can't walk through walls")
+            commands[(input("What would you like to do?\n")).lower()]()
+            return
 
 def GenerateRoomLayouts(Corridor):
     ReRunRoomLayout = True
@@ -103,7 +122,7 @@ def PlayBlackJack():
 
 
 rooms  = [[Room1_0,Room1_1,Room1_2,Room1_3],[Room2_0,Room2_1,Room2_2,Room2_3],[Room3_0,Room3_1,Room3_2,Room3_3]]
-commands = {"go back":GoBack,"enter room":EnterRoom}
+commands = {"go back":GoBack,"enter a room":EnterRoom}
 
 # setting 1 is the train, 2 is the haunted house and 3 is the cave.
 
@@ -112,12 +131,12 @@ setting = random.randint(1,3)
 GenerateRoomLayouts(1)
 GenerateRoomLayouts(2)
 GenerateRoomLayouts(3)
-
-
+endCode = str(random.randint(1,9)),str(random.randint(1,9)),str(random.randint(1,9)),str(random.randint(1,9))
 
 name = input("What is your name young adventurer?\n")
-print("When playing this game there are" + len(commands) + "commands that you can use.")
+print("When playing this game there are" + str(len(commands)) + "commands that you can use.")
 print("Go Back, this will let you leave a room or corridor/ junction.")
-print("Enter room, this will allow you to enter a room, if you have the room number.")
+print("Enter a room, this will allow you to enter a room, if you have the room number.")
 print("Attack, this will let you attack monsters")
-rooms[1][0]()
+
+rooms[0][0]()
